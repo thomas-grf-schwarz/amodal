b7379ceab6c1306907cddb603ffdac7915532dad
thomas-grf-schwarz <thomas.schwarz@rolmail.net>
2025-03-10 10:34:59 +0100

    move patch size into data utils

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+import collections
+
 class TransformedDataset:
 
     def __init__(self, dataset, transform):
@@ -36,3 +38,24 @@ def transforms(x, steps):
     for step in steps:
         x = step(x)
     return x
+
+
+class PatchSize(collections.abc.Iterable):
+
+    def __init__(self, sizes):
+        self.sizes = sizes
+
+    def __getitem__(self, idx):
+        return self.sizes[idx]
+
+    def __pow__(self, power):
+        """
+        handle non-square images
+        """
+        product = 1
+        for size in self.sizes:
+            product *= size
+        return product
+
+    def __iter__(self):
+        return iter(self.sizes)
